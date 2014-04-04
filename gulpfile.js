738f6164da68f07817930724d9de8ae74fa73b69
Christopher Knötschke <cknoetschke@gmail.com>
2014-04-04 11:34:42 +0200

    added own jshint error reporter and exitting the process with code 1 on errors, so the test will fail

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var
-	gulp 	= require('gulp'),
-	mocha 	= require('gulp-mocha'),
-	jshint 	= require('gulp-jshint'),
-	jscs 	= require('gulp-jscs')
+	util         = require('util'),
+	gulp         = require('gulp'),
+	mocha        = require('gulp-mocha'),
+	jshint       = require('gulp-jshint'),
+	jscs         = require('gulp-jscs'),
+	map          = require('gulp-jshint/node_modules/map-stream'),
+	jshintErrors = []
 ;
 
 gulp.task('validate', function() {
 	return gulp.src([__filename, './index.js', './lib/**/*.js', './test/**/test_*.js'])
 		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
+		.pipe(map(function (file, cb) {
+			if (file.jshint && !file.jshint.success) {
+				jshintErrors.push.apply(jshintErrors, file.jshint.results);
+			}
+
+			cb(null, file);
+		}))
 		.pipe(jscs(__dirname + '/.jscs.json'));
 });
 
-gulp.task('test', function() {
+gulp.task('test', ['validate'], function() {
 	return gulp.src('./test/**/test_*.js')
 		.pipe(mocha({
 			resporter: 'spec',
@@ -20,4 +29,11 @@ gulp.task('test', function() {
 		}));
 });
 
-gulp.task('default', ['validate', 'test']);
+gulp.task('default', ['test'], function() {
+	if (jshintErrors.length) {
+		console.error(jshintErrors.map(function(error) {
+			return util.format('%s\nLine: %d\nMessage: %s\n\n', error.file, error.error.line, error.error.reason);
+		}).join(''));
+		process.exit(1);
+	}
+});
