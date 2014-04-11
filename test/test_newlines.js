bb32f219dc6dfe04be0340495b0659dee2be20f9
Christopher Knötschke <cknoetschke@gmail.com>
2014-04-11 14:40:01 +0200

    added test for newlineMaximum

--- test/test_newlines.js
+++ test/test_newlines.js
@@ -7,7 +7,8 @@ require('should');
 
 describe('newlines', function() {
 	var validator = new Validator({
-		newline: true
+		newline: true,
+		newlineMaximum: 3
 	});
 
 	it('fixtures/newlines/valid.txt should have no errors', function() {
@@ -29,5 +30,17 @@ describe('newlines', function() {
 
 		validator.validate(p);
 		validator.getInvalidLines(p).should.not.be.empty;
+		Object.keys(validator.getInvalidLines(p)).should.be.eql(['8', '17', '31']);
+	});
+
+	it('should throw an exception if newlineMaximum is not greater than 0', function() {
+		var
+			p = __filename,
+			validator = new Validator({
+				newlineMaximum: 0
+			})
+		;
+
+		validator.validate.bind(validator, p).should.throw(/for the maximum of newlines is invalid/);
 	});
 });
