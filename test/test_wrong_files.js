0274dcc179c8a46a2b31d7c5db67e0c66791a855
Christopher Knötschke <cknoetschke@gmail.com>
2014-04-11 12:50:47 +0200

    added tests for wrong files

---
+++ test/test_wrong_files.js
@@ -0,0 +1,44 @@
+var
+	path = require('path'),
+	Validator = require('../index')
+;
+
+require('should');
+
+describe('wrong files', function() {
+	var validator = new Validator();
+
+	it('should throw the exception "{a} does not exists" if path does not exists', function() {
+		var p = "/this/file/does/not/exists";
+		validator.validate.bind(validator, p).should.throw(/does not exists/);
+	});
+
+	it('should throw the exception "{a} is not a file" if path is a directory', function() {
+		var p = __dirname;
+		validator.validate.bind(validator, p).should.throw(/is not a file/);
+	});
+
+	it('should throw the exception "config file wasn\'t found" if editorconfig does not exists', function() {
+		var
+			p = "/this/file/does/not/exists",
+			validator = new Validator({
+				editorconfig: p
+			})
+		;
+
+		validator._path = p;
+		validator._loadSettings.bind(validator).should.throw(/config file .+? wasn't found/);
+	});
+
+	it('should throw the exception "{a} is not a file" if editorconfig is a directory', function() {
+		var
+			p = __dirname,
+			validator = new Validator({
+				editorconfig: p
+			})
+		;
+
+		validator._path = p;
+		validator._loadSettings.bind(validator).should.throw(/is not a file/);
+	});
+});
