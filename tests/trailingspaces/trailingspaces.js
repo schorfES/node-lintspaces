5511b7e1dca7302d3138bb4a457301bfc3f27682
Norman Rusch <norman.rusch@moccu.com>
2014-07-10 17:13:31 +0200

    Add option to ignore trailingspaces

--- tests/trailingspaces/trailingspaces.js
+++ tests/trailingspaces/trailingspaces.js
@@ -33,5 +33,31 @@ exports.tests = {
 
 		test.deepEqual({}, report);
 		test.done();
+	},
+
+	'should ignore trailingspaces inside comments when option is set': function(test) {
+		// this should fail because there is one invalid line and there
+		// are also trainingspaces
+		file = __dirname + '/fixures/ignores.js';
+		validator = new Validator({
+			trailingspaces: true,
+			trailingspacesToIgnores: true,
+			ignores: ['js-comments']
+		});
+
+		validator.validate(file);
+		report = validator.getInvalidFiles();
+		expected = {};
+		expected[file] = {
+			'9': [merge({}, Messages.TRAILINGSPACES, {line: 9})], // singleline
+			'11': [merge({}, Messages.TRAILINGSPACES, {line: 11})], // singleline
+			'15': [merge({}, Messages.TRAILINGSPACES, {line: 15})], // multiline
+			'17': [merge({}, Messages.TRAILINGSPACES, {line: 17})], // singleline
+			'21': [merge({}, Messages.TRAILINGSPACES, {line: 21})], // multiline
+			'26': [merge({}, Messages.TRAILINGSPACES, {line: 26})] // multiline
+		};
+
+		test.deepEqual(expected, report);
+		test.done();
 	}
 };
